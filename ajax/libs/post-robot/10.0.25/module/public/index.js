11f900854adee716520559497c8d0de60e006b72
robocdnjs <cdnjs-github@cloudflare.com>
2020-04-19 18:14:15 +0000

    Add post-robot v10.0.25

---
+++ ajax/libs/post-robot/10.0.25/module/public/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+exports.__esModule = true;
+
+var _on = require("./on");
+
+Object.keys(_on).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  exports[key] = _on[key];
+});
+
+var _send = require("./send");
+
+Object.keys(_send).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  exports[key] = _send[key];
+});

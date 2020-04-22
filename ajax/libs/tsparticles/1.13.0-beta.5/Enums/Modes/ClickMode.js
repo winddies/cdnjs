2a30f7212a6d13f994ae385d7a46235aa19de24c
robocdnjs <cdnjs-github@cloudflare.com>
2020-04-22 18:04:37 +0000

    Add tsparticles v1.13.0-beta.5

---
+++ ajax/libs/tsparticles/1.13.0-beta.5/Enums/Modes/ClickMode.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ClickMode;
+(function (ClickMode) {
+    ClickMode["bubble"] = "bubble";
+    ClickMode["push"] = "push";
+    ClickMode["remove"] = "remove";
+    ClickMode["repulse"] = "repulse";
+    ClickMode["emitter"] = "emitter";
+})(ClickMode = exports.ClickMode || (exports.ClickMode = {}));
